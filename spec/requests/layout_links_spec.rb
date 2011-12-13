658ed67759c7555dc49fc3e46721a36efd25c8f8
logan sease <lsease@vt.edu>
2011-12-13 10:01:37 -0500

    sign up link added files

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -2,9 +2,17 @@ require 'spec_helper'
 
 describe "LayoutLinks" do
  
-  ##it "should have a Home Page at '/'"  do
-  ##  get '/'
-  ##  response.should have_selector('title', :content => "Home") 
-  ##end
+ # it "should have a Home Page at '/'"  do
+ #   get '/'
+ #   response.should have_selector('title', :content => "Home") 
+ # end
+ 
+## it "should have a sign up page at /signup"
+##   get '/signup'
+##   response.should have_selector('title', :content => 'signup')
+## end
+
+  it "should have the right links on the layout" do
+  end
 
 end
