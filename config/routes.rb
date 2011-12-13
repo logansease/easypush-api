ed9c7f9c220b1238fd8d653b78099dab3704826f
logan sease <lsease@vt.edu>
2011-12-13 09:33:37 -0500

    layout links logo helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
-SampleApp::Application.routes.draw do
-  get "pages/home"
-
-  get "pages/contact" 
+SampleApp::Application.routes.draw do  
+ 
+  root :to => "pages#home" 
   
-  get "pages/about"  
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'   
+  match '/help', :to => 'pages#help' 
+  get "pages/home"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe "LayoutLinks" do
+ 
+  ##it "should have a Home Page at '/'"  do
+  ##  get '/'
+  ##  response.should have_selector('title', :content => "Home") 
+  ##end
+
+end
