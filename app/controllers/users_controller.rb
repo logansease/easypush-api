4c77bf9d00f63f1ab3abaeaa3c7f1631fc04ec40
logan sease <lsease@vt.edu>
2011-12-13 12:33:09 -0500

    added user stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   def new 
     @title = "Sign up"
+  end  
+  
+  def show               
+     id = params[:id]
+     @user = User.find(id)
   end
 
 end
