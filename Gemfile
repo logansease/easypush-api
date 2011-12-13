3c0c3a93383bb8a888f1639bc52f612b9b9d9808
logan sease <lsease@vt.edu>
2011-12-13 10:38:22 -0500

    fixed int tests

--- Gemfile
+++ Gemfile
@@ -5,7 +5,9 @@ gem 'rails', '3.0.1'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'sqlite3-ruby', '1.2.5', :require => 'sqlite3'
+gem 'sqlite3-ruby', '1.2.5', :require => 'sqlite3'   
+
+gem 'webrat', '0.7.1'
 
 # Bundle gems for the local environment. Make sure to
 # put test-only gems in this group so their generators
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -2,17 +2,31 @@ require 'spec_helper'
 
 describe "LayoutLinks" do
  
- # it "should have a Home Page at '/'"  do
- #   get '/'
- #   response.should have_selector('title', :content => "Home") 
- # end
  
-## it "should have a sign up page at /signup"
-##   get '/signup'
-##   response.should have_selector('title', :content => 'signup')
-## end
-
-  it "should have the right links on the layout" do
-  end
+ describe "test links" do
+   it "should have a home page at '/' " do
+     get '/'
+     response.should have_selector('title', :content => "Home")
+   end                            
+   
+   it "should have a sign up page at /signup" do
+      get "/signup"
+      response.should have_selector('title', :content => 'Sign up')
+   end 
+   
+   it "should have correct links int he layout" do
+     visit root_path
+     response.should have_selector('title', :content => "Home")
+     click_link "About"
+     response.should have_selector('title', :content=> "About")        
+     click_link "Contact"
+     response.should have_selector('title', :content=> "Contact")  
+     click_link "Home"
+     response.should have_selector('title', :content=> "Home")
+     click_link "Sign up now!"
+     response.should have_selector('title', :content=> "Sign up")    
+     response.should have_selector('a[href="/"]>img')
+   end
+ end
 
 end
