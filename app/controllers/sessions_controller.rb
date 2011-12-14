ed06b550c3b08a68375048296a940fd898a93f65
logan sease <lsease@vt.edu>
2011-12-14 17:18:12 -0500

    session controller add and sign in view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,12 @@ class SessionsController < ApplicationController
   
   def create 
     user = User.authenticate(params[:session][:email], params[:session][:password])
-    if user.nil
-      #render 'new'
+    if user.nil?          
+      @title = "Sign in"
+      flash.now[:error] = "Invalid email / password combination"       
+      #note the flash.now makes the error persist only now, NOT for the next request,
+      #since as noted below we are not moving to a new request
+      render 'new'  #note this is not a new request
     else
       #successful sign in
     end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -25,12 +25,23 @@ describe SessionsController do
         end
         
        it "should re-render submission page" do
+           post :create, :session => @attr   
+           response.should render_template('new')
+       end        
+       
+       it "should have an error message" do
            post :create, :session => @attr
+           flash.now[:error].should =~ /invalid/i
+       end      
+       
+       it "should have the right title" do
+         post :create, :session => @attr
+         response.should have_selector('title', :content => "Sign in")
        end
        
      end
      
-     describe success do
+     describe "success" do
        
      end
   end
