91df63c645f33dbbac5c0ea65355b275a9e6ff34
Logan Sease <lsease@gmail.com>
2012-06-01 16:21:03 -0400

    update to 3.1.3

--- Gemfile
+++ Gemfile
@@ -1,18 +1,24 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.0.11'
+gem 'rails', '3.1.3'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'sqlite3-ruby', '1.2.5', :require => 'sqlite3'   
-gem 'webrat', '0.7.1'       
+gem 'sqlite3', '>= 1.3.1', :require => 'sqlite3'
+gem 'webrat', '0.7.1'
 gem 'gravatar_image_tag', '0.1.0'  
 gem 'will_paginate', '3.0.pre' 
 gem 'faker', '0.3.1'
 gem 'json'
 gem 'koala', '1.3.0'
 gem 'jquery-rails'
+
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
 
 # Bundle gems for the local environment. Make sure to
 # put test-only gems in this group so their generators
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,6 +12,22 @@ SampleApp::Application.configure do
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
+  # config/environments/production.rb
+        # Compress JavaScripts and CSS
+        config.assets.compress = true
+
+        # Don't fallback to assets pipeline if a precompiled asset is missed
+        config.assets.compile = false
+
+        # Generate digests for assets URLs
+        config.assets.digest = true
+
+        # Defaults to Rails.root.join("public/assets")
+        # config.assets.manifest = YOUR_PATH
+
+        config.assets.js_compressor  = :uglifier
+        config.assets.css_compressor = :scss
+
   # For nginx:
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
