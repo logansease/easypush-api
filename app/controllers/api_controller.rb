c1840e2de6df8c19d7542025a054e442482ada87
Logan Sease <lsease@gmail.com>
2012-10-15 13:13:39 -0400

    push not

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -117,9 +117,11 @@ class ApiController < ApplicationController
     end
 
     appNotifications =  PushNotificationId.find_by_app_id(app_id)
-    if(appNotifications && !appNotifications.include?(device))
+    if(appNotifications || !appNotifications.include?(device))
       device = PushNotificationId.create!(:app_id => app_id, :device_id => device, :fb_user_id => fb_user)
     end
+
+    render "success"
 
   end
 
