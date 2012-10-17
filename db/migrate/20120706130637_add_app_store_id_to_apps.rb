c7febaa8b707e1bd5e23ffe266085b965a50984c
Logan Sease <lsease@gmail.com>
2012-10-17 09:42:02 -0400

    removed migrations

--- db/migrate/20120706130637_add_app_store_id_to_apps.rb
+++
@@ -1,5 +0,0 @@
-class AddAppStoreIdToApps < ActiveRecord::Migration
-  def change
-    add_column :apps, :app_store_id, :string
-  end
-end
--- db/migrate/20121016180022_add_push_to_apps.rb
+++
@@ -1,6 +0,0 @@
-class AddPushToApps < ActiveRecord::Migration
-  def change
-    add_column :apps, :dev_push_cert, :string
-    add_column :apps, :prod_push_cert, :string
-  end
-end
